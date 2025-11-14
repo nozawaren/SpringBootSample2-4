3cdb4c0cbc0ce0f7098a1a60f769eeead77cc80b
nozawaren <renzu20000@gmail.com>
2025-11-14 21:10:36 +0900

    解体新書2週目6章

--- SpringBootSample2-4/src/main/java/com/example/form/SignupForm.java
+++ SpringBootSample2-4/src/main/java/com/example/form/SignupForm.java
@@ -17,26 +17,39 @@ import lombok.Data;
 @Data
 public class SignupForm {
 
+	//ValidGroup1→ValidGroup2の順でバリテーションを実行するように設定する
+
+	//ブランクでないことをチェックする
 	@NotBlank(groups = ValidGroup1.class)
+	//メールアドレスであるかをチェックする
 	@Email(groups = ValidGroup2.class)
 	private String userId;
 
+	//ブランクでないことをチェックする	
 	@NotBlank(groups = ValidGroup1.class)
+	//入力内容の長さが4文字以上100文字以下であることをチェックする
 	@Length(min = 4, max = 100, groups = ValidGroup2.class)
+	//入力内容が英字（大文字、小文字）と数字であることをチェックする
 	@Pattern(regexp = "^[a-zA-Z0-9]+$", groups = ValidGroup2.class)
 	private String password;
 
+	//ブランクでないことをチェックする	
 	@NotBlank(groups = ValidGroup1.class)
 	private String userName;
 
+	//入力された内容をyyyy/MM/ddの形に変換する
 	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	//nullでないことをチェックする	
 	@NotNull(groups = ValidGroup1.class)
 	private Date birthday;
 
+	//入力内容が20文字以上であることをチェックする
 	@Min(value = 20, groups = ValidGroup2.class)
+	//入力内容が100文字以下であることをチェックする
 	@Max(value = 100, groups = ValidGroup2.class)
 	private Integer age;
 
+	//nullでないことをチェックする	
 	@NotNull(groups = ValidGroup1.class)
 	private Integer gender;
 
